bcf586c4f1c8bc81c211a301dba3c3da34ee1b65
Martin <martin.heinrich91@gmail.com>
2024-04-23 19:55:06 +0200

    Finished readability.c and added readability-improved with beter code structure.

--- readability-improved.c
+++ readability-improved.c
@@ -1,19 +1,18 @@
 #include <ctype.h>
+#include <cs50.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <cs50.h>
 
-void counter(int length, string text);
-float return_index(float x, float y, float z);
+float return_index(int numbers []);
 void print_grade(float index);
-
-float number_of_letters = 0;
-float number_of_words = 0;
-float number_of_sentences = 0;
+int* counter_array(int length, string text, int numbers []);
 
 int main(void)
 {
+    int letters = 0, words = 0, sentences = 0;
+    int numbers [] = {letters, words, sentences};
+
     // Prompt the user for some text
     // Remember string is array of characters
 
@@ -21,82 +20,63 @@ int main(void)
 
     int length = strlen(text);
 
-    counter(length, text);
-
-/*     printf("number of letters = %f\n", number_of_letters);
-    printf("number of words = %f\n", number_of_words);
-    printf("number of sentences = %f \n", number_of_sentences); */
-
-    float index = return_index(number_of_letters, number_of_words, number_of_sentences);
+    float index = return_index(counter_array(length, text, numbers));
 
     print_grade(index);
 
-    //printf("%f", index_x);
-
-/*     if(index(number_of_letters, number_of_words, number_of_sentences) < 1){
-        printf("less than 1");
-    } */
-
-    //printf("%i", length);
-
-
-    // Count the number of letters, words, and sentences in the text
-    // I  want to skip empty spaces while counting words and sentences
-    // word is considered to be any sequence of characters (to count number of words you can maybe count spaces. For example count spaces +1)
-
-    // I  can use isspace from sc50.h to check if it is alphabetical to handle empty spaces in string
-    // I  can use ispunct from sc50.h to check if it is punctuation to handle empty spaces in string
-
-    // I assume I would count any period, exclamation point, period or question marks as 1 sentence???
-
-    // Print the grade level
 }
 
-void counter(int length, string text)
+int* counter_array(int length, string text, int numbers [])
 {
-
     int x = 0;
 
     while (x <= length)
     {
         if (isalpha(text[x]) != 0)
         {
-            number_of_letters += 1;
+            numbers[0] += 1;
         }
         else if (isblank(text[x]) != 0 || text[x] == 0)
         {
-            number_of_words += 1;
+            numbers[1] += 1;
         }
         else if (text[x] == '.' || text[x] == '?' || text[x] == '!')
         {
-            number_of_sentences += 1;
+            numbers[2] += 1;
         }
         x++;
     }
+
+    return numbers;
 }
 
-float return_index(float x, float y, float z)
+float return_index(int numbers [])
 {
-    if (x <= 0 || y <= 0 || z <= 0)
+    if (numbers[0] <= 0 || numbers[1] <= 0 || numbers[2] <= 0)
     {
-        return 0;
+        return 1;
     }
+
+    float x = (float) numbers[0];
+    float y = (float) numbers[1];
+    float z = (float) numbers[2];
 
     // Compute the Coleman-Liau index
     float index = 0;
 
     // where L is the average number of letters per 100 words in the text,
     float L = (x / y) * 100;
-/*     printf("L = %f\n", L);
- */
+
     //and S is the average number of sentences per 100 words in the text.
     float S = (z / y) * 100;
-/*     printf("S = %f\n", S);
- */
 
     // index = 0.0588 * L - 0.296 * S - 15.8
 
     index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index <= 1){
+        index = 1;
+    }
 
     return roundf(index);
 }
