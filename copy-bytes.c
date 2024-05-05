37aa37af228b23255840fdb6d8af2ff90bce7fa8
Martin <martin.heinrich91@gmail.com>
2024-05-05 20:20:13 +0200

    Added copy-bytes.c

---
+++ copy-bytes.c
@@ -0,0 +1,24 @@
+// Copies a file
+
+#include <stdio.h>
+#include <stdint.h>
+
+// unsigned int, 8 bytes of positive ints
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+    FILE *src = fopen(argv[1], "rb");
+    FILE *dst = fopen(argv[2], "wb");
+
+    BYTE b;
+
+    // fread reads bytes from a file
+    while (fread(&b, sizeof(b), 1, src) != 0)
+    {
+        fwrite(&b, sizeof(b), 1, dst);
+    }
+
+    fclose(dst);
+    fclose(src);
+}
