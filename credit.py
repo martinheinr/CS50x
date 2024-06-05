986d13b1a1ccd426169246c6e78b78ce13c26967
Martin <martin.heinrich91@gmail.com>
2024-06-05 20:58:16 +0200

    complete credit.py

--- credit.py
+++ credit.py
@@ -8,18 +8,20 @@ def main():
         if int(card_number) > 0:
             break
 
-    print(luhns_algorithm(card_number))
+    checksum = luhns_algorithm(card_number)
+    validate(checksum, card_number)
+
 
 def luhns_algorithm(card_number):
     sum_of_multiplied = 0
     sum_of_not_multiplied = 0
 
-    for element in str(card_number[ : :-2]):
+    for element in str(card_number[::-2]):
         sum_of_not_multiplied = sum_of_not_multiplied + int(element)
 
     card_number = card_number[:-1]
 
-    for element in str(card_number[ : :-2]):
+    for element in str(card_number[::-2]):
         digit = int(element) * 2
         if digit > 9:
             digit = (digit % 10) + 1
@@ -33,5 +35,31 @@ def luhns_algorithm(card_number):
     else:
         return False
 
-main()
 
+def validate(checksum, card_number):
+
+    first_digit = card_number[0]
+    second_digit = card_number[1]
+
+    if checksum == False:
+        print("INVALID")
+
+    while checksum:
+        if first_digit == "4" and (len(card_number) == 13 or len(card_number) == 16):
+            print("VISA")
+            break
+
+        elif first_digit == "3" and (second_digit == "4" or second_digit == "7"):
+            print("AMEX")
+            break
+
+        elif first_digit == "5" and (second_digit > "0" and second_digit < "6"):
+            print("MASTERCARD")
+            break
+
+        else:
+            print("INVALID")
+            break
+
+
+main()
